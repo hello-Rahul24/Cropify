8900c31eddf612d982c1907e5b2d4c2ed8bc45bf
Rahul Giri <girirahul2028@gmail.com>
2026-01-13 00:56:12 +0530

    fixed version

--- app/lib/earthEngine.ts
+++ app/lib/earthEngine.ts
@@ -5,14 +5,19 @@ let initialized = false;
 
 export async function initEarthEngine() {
   if (initialized) return;
-console.log("EE key path:", process.env.GOOGLE_APPLICATION_CREDENTIALS);
+  //console.log("EE key path:", process.env.GOOGLE_APPLICATION_CREDENTIALS);
 
   return new Promise<void>((resolve, reject) => {
     try {
-      const key = JSON.parse(
-        fs.readFileSync(process.env.GOOGLE_APPLICATION_CREDENTIALS!, "utf8")
-      );
+      const cred = process.env.GOOGLE_APPLICATION_CREDENTIALS;
 
+      if (!cred) {
+        throw new Error("GOOGLE_APPLICATION_CREDENTIALS is not set");
+      }
+
+      const key = cred.trim().startsWith("{")
+        ? JSON.parse(cred) // ✅ Vercel (JSON string)
+        : JSON.parse(fs.readFileSync(cred, "utf8")); // ✅ Local (file path)
       ee.data.authenticateViaPrivateKey(
         key,
         () => {
